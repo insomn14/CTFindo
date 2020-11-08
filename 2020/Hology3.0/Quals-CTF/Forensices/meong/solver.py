d1b964f57cbec5b12eb55d5075c2f5a0fc09b60a
insomn14 <barirehatta@gmail.com>
2020-11-08 07:10:33 +0700

    Solved

---
+++ 2020/Hology3.0/Quals-CTF/Forensices/meong/solver.py
@@ -0,0 +1,4 @@
+from base64 import b85decode
+
+print(b85decode('/N,4A0JYIF1+0K1UB0fV&XP*N1c[M'.encode())) # -6.7053554, 108.5553879 -> cirebon
+print(b85decode('/N,4?0JtXD2)Hj,0ebR>2)R6I3&`h'.encode())) # -6.902492, 107.61877498 -> bandung
