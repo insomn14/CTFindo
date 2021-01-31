535f4773a1751b6b8d39810f8f9e24581e7bd167
insomn14 <barirehatta@gmail.com>
2021-02-01 01:38:41 +0700

    Update solver.py

--- 2021/Arkavidia7/Reverse Engineering/Oncle Jazz/solver.py
+++ 2021/Arkavidia7/Reverse Engineering/Oncle Jazz/solver.py
@@ -4,13 +4,13 @@ mapping = {
 from pwn import xor
 
 k = b'oncle'
-part1 = xor(bytes.fromhex('0A0B3C011B3A090E010C0909141558130D081C2E')[::-1], k)
+part1 = xor(bytes.fromhex('0A0B3C011B3A090E010C0909141558130D081C2E')[::-1], k).decode('utf-8')
 
 stack = 'pyogzyuj'[::-1]
 stack += 'jmgtueeeeeec'
 part2 = ''
-while len(part2) != len(target):
+while len(part2) != len(stack):
 	for m in mapping.keys():
-		if mapping[m] == target[len(part2)]:
+		if mapping[m] == stack[len(part2)]:
 			part2 += m
 print(part1+part2)
